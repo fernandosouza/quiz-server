3e172a8a5146f985bc7a606d604c88528dd00ada
Fernando Souza <nandosouzafilho@gmail.com>
2017-11-19 13:52:42 +0100

    SF

--- answers.js
+++ answers.js
@@ -5,12 +5,12 @@ const Answer = sequelize.define('answer', {
   text: {
     type: Sequelize.STRING
   }
-});
+})
   
 Answer.sync({force: true}).then(() => {
   return Answer.create({
     text: 'My First Answer'
   });
-});
+})
 
 module.exports = Answer;
